f7f78e7b15a0f10261272fca19069ab7796eb6c1
Younglong <kyl8555@gmail.com>
2024-06-28 06:51:45 +0900

    feat: entity 추가

--- src/main/java/io/hhplus/architecture/lecture/domain/Lecture.java
+++ src/main/java/io/hhplus/architecture/lecture/domain/Lecture.java
@@ -1,4 +1,38 @@
 package io.hhplus.architecture.lecture.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Lecture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String speaker;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public Lecture(Long id, String title, String speaker) {
+        this.id = id;
+        this.title = title;
+        this.speaker = speaker;
+    }
 }
